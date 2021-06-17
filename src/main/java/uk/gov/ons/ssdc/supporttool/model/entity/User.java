9d6d921f5786d8e8b0d1445f84a2a46213f0835b
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-06-17 15:48:11 +0100

    Fix user reserved word

--- src/main/java/uk/gov/ons/ssdc/supporttool/model/entity/User.java
+++ src/main/java/uk/gov/ons/ssdc/supporttool/model/entity/User.java
@@ -9,10 +9,12 @@ import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "users")
 public class User {
   @Id private UUID id;
 
