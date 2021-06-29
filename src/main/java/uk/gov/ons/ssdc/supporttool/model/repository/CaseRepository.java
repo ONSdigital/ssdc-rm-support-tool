8319ab7c1017e0fcc14b31530c01e83f7b3708c8
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-06-29 15:41:12 +0100

    Fix param thing

--- src/main/java/uk/gov/ons/ssdc/supporttool/model/repository/CaseRepository.java
+++ src/main/java/uk/gov/ons/ssdc/supporttool/model/repository/CaseRepository.java
@@ -4,16 +4,18 @@ import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import uk.gov.ons.ssdc.supporttool.model.entity.Case;
 
 @RepositoryRestResource
 public interface CaseRepository extends PagingAndSortingRepository<Case, UUID> {
-  Optional<Case> findByCaseRef(Long caseRef);
+  Optional<Case> findByCaseRef(@Param("caseRef") Long caseRef);
 
   @Query(
       value =
           "SELECT * FROM casev3.cases WHERE collection_exercise_id = :collexId AND UPPER(REPLACE(sample ->> :key, ' ', '')) LIKE CONCAT('%', UPPER(REPLACE(:value, ' ', '')), '%')",
       nativeQuery = true)
-  Iterable<Case> findBySampleContains(UUID collexId, String key, String value);
+  Iterable<Case> findBySampleContains(
+      @Param("collexId") UUID collexId, @Param("key") String key, @Param("value") String value);
 }
