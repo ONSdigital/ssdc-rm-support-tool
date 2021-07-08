93d2a7efd985f2d393c21f919b4d4ae371a2c4c0
AdamHawtin <adam.hawtin@ons.gov.uk>
2021-07-08 13:18:31 +0100

    Implement all filters

--- src/main/java/uk/gov/ons/ssdc/supporttool/endpoint/CaseRowMapper.java
+++ src/main/java/uk/gov/ons/ssdc/supporttool/endpoint/CaseRowMapper.java
@@ -9,25 +9,25 @@ import uk.gov.ons.ssdc.supporttool.model.dto.CaseContainerDto;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Map;
+import java.util.UUID;
 
 //Needs moving somewhere
 @Component
 public class CaseRowMapper implements RowMapper<CaseContainerDto> {
-    @SneakyThrows
-    @Override
-    public CaseContainerDto mapRow(ResultSet resultSet, int rowNum) throws SQLException {
-        CaseContainerDto caseContainerDto = new CaseContainerDto();
-//         caseContainerDto.setId(resultSet.getString("id"));
-        caseContainerDto.setCaseRef(resultSet.getString("case_ref"));
-        ObjectMapper objectMapper = new ObjectMapper();
-        caseContainerDto.setSample(objectMapper.readValue(resultSet.getString("sample"), Map.class));
+  private static final ObjectMapper objectMapper = new ObjectMapper();
+
+  @SneakyThrows
+  @Override
+  public CaseContainerDto mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+    CaseContainerDto caseContainerDto = new CaseContainerDto();
+    caseContainerDto.setId(resultSet.getObject("id", UUID.class));
+    caseContainerDto.setCaseRef(resultSet.getString("case_ref"));
+    caseContainerDto.setSurveyLaunched(resultSet.getObject("survey_launched", boolean.class));
+    caseContainerDto.setReceiptReceived(resultSet.getObject("receipt_received", boolean.class));
+    caseContainerDto.setAddressInvalid(resultSet.getObject("address_invalid", boolean.class));
+    caseContainerDto.setSample(objectMapper.readValue(resultSet.getString("sample"), Map.class));
 
 
-//        String sample = resultSet.getString("sample");
-
-//        caseContainerDto.setSample(resultSet.get("sample"));
-
-
-        return caseContainerDto;
-    }
+    return caseContainerDto;
+  }
 }
--- src/main/java/uk/gov/ons/ssdc/supporttool/endpoint/CasesEndpoint.java
+++ src/main/java/uk/gov/ons/ssdc/supporttool/endpoint/CasesEndpoint.java
@@ -1,18 +1,13 @@
 package uk.gov.ons.ssdc.supporttool.endpoint;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +15,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.ons.ssdc.supporttool.model.dto.CaseContainerDto;
-import uk.gov.ons.ssdc.supporttool.model.entity.Case;
-import uk.gov.ons.ssdc.supporttool.model.repository.CaseRepository;
 
 @RestController
 @RequestMapping(value = "/cases")
@@ -41,23 +34,20 @@ public class CasesEndpoint {
 
   @GetMapping(value = "/search")
   @ResponseBody
-  public ResponseEntity searchCasesBySampleData(
+  public List<CaseContainerDto> searchCasesBySampleData(
       @RequestParam(value = "searchTerm") String searchTerm,
-      // TODO make survey required
       @RequestParam(value = "surveyId") UUID surveyId,
       @RequestParam(value = "collexId", required = false) UUID collexId,
       @RequestParam(value = "receipted", required = false) Boolean receiptReceived,
-      @RequestParam(value = "invalid", required = false) Boolean invalidAddress,
+      @RequestParam(value = "invalid", required = false) Boolean addressInvalid,
       @RequestParam(value = "launched", required = false) Boolean surveyLaunched,
-      @RequestParam(value = "refusal", required = false) String refusalReceived) throws JsonProcessingException {
+      @RequestParam(value = "refusal", required = false) String refusalReceived) {
 
-    //        Do we build the blooming query dynamically
-    //        can possibly make with if null else
 
-    if (refusalReceived != null) {
-      if (refusalReceived.equals("null")) {
-        refusalReceived = null;
-      }
+    if (refusalReceived != null && refusalReceived.equals("null")) {
+      // We want to be able to search for cases where refusal received is null by supplying the string "null"
+      // If no refusalReceived query string is provided at all then we do not filter at all
+      refusalReceived = null;
     }
 
     searchTerm = '%' + searchTerm + '%';
@@ -68,7 +58,7 @@ public class CasesEndpoint {
             + " AND ce.survey_id = :surveyId"
             + " AND EXISTS (SELECT * FROM jsonb_each_text(ca.sample) AS x(ky, val) WHERE lower(x.val) LIKE lower(:searchTerm))";
 
-    Map namedParameters = new HashMap();
+    Map<String, Object> namedParameters = new HashMap();
     namedParameters.put("surveyId", surveyId);
     namedParameters.put("searchTerm", searchTerm);
 
@@ -76,36 +66,29 @@ public class CasesEndpoint {
       query += " AND ce.id = :collexId";
       namedParameters.put("collexId", collexId);
     }
+    if (receiptReceived != null) {
+      query += " AND ca.receipt_received = :receiptReceived";
+      namedParameters.put("receiptReceived", receiptReceived);
+    }
+
+    if (addressInvalid != null) {
+      query += " AND ca.address_invalid = :addressInvalid";
+      namedParameters.put("addressInvalid", addressInvalid);
+    }
+
+    if (surveyLaunched != null) {
+      query += " AND ca.survey_launched = :surveyLaunched";
+      namedParameters.put("surveyLaunched", surveyLaunched);
+    }
+
+    if (refusalReceived != null) {
+      query += " AND ca.refusal_received = :refusalReceived";
+      namedParameters.put("refusalReceiced", refusalReceived);
+    }
 
 
-    List<CaseContainerDto> cases = namedParameterJdbcTemplate.query(query,
-            namedParameters,
-            caseRowMapper);
-
-//    List<Map<String, Object>> result = namedParameterJdbcTemplate.queryForList(query, namedParameters);
-//    ObjectMapper objectMapper = new ObjectMapper();
-//    final String str = objectMapper.writeValueAsString(result);
-    var headers = new HttpHeaders();
-    headers.add("Responded", "MyController");
-
-//    Not sure about headers, ignore?  Accepted is that 202? want 200?
-    return ResponseEntity.accepted().headers(headers).body(cases);
-
-    //
-    //    query = "SELECT ca.id, ca.case_ref, ca.sample, ca.address_invalid, ca.receipt_received,
-    // ca.refusal_received" +
-    //        " FROM casev3.cases ca, casev3.collection_exercise ce WHERE ca.collection_exercise_id
-    // = ce.id" +
-    //        " AND ce.survey_id = ?" +
-    //        " AND EXISTS (SELECT * FROM jsonb_each_text(ca.sample) AS x(ky, val) WHERE
-    // lower(x.val) LIKE lower(?))";
-    //    ArrayList<Object> args = new ArrayList<>();
-    //    args.add(surveyId);
-    //    args.add(searchTerm);
-    //
-    //
-    //    List<Map<String, Object>> results = jdbcTemplate.queryForList(query, args);
-
-    //        Make iterable
+    return namedParameterJdbcTemplate.query(query,
+        namedParameters,
+        caseRowMapper);
   }
 }
