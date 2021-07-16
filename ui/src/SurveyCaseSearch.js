b2faa27e15368a0251b816e6db679306a6caf7c4
AdamHawtin <adam.hawtin@ons.gov.uk>
2021-07-16 13:16:46 +0100

    Hide spinner if search errors

--- ui/src/SurveyCaseSearch.js
+++ ui/src/SurveyCaseSearch.js
@@ -40,6 +40,7 @@ class SurveyCaseSearch extends Component {
     // TODO: We need more elegant error handling throughout the whole application, but this will at least protect temporarily
     if (!response.ok) {
       alert(`Error: ${response.state}`);
+      this.setState({ isWaitingForResults: false });
       return;
     }
 
