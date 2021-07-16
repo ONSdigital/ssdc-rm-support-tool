170b794e1cd0066227b07f0dc47423f5b42933c8
AdamHawtin <adam.hawtin@ons.gov.uk>
2021-07-16 13:32:23 +0100

    Remove dead code

--- ui/src/SurveyDetails.js
+++ ui/src/SurveyDetails.js
@@ -35,7 +35,6 @@ class SurveyDetails extends Component {
     allowableFulfilmentPrintTemplates: [],
     printTemplateToAllow: '',
     printTemplateValidationError: false,
-    matchedCases: [],
   }
 
   componentDidMount() {
