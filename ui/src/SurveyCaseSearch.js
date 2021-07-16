7b781a37c4b13d2b974994641b950be14ded5ff8
AdamHawtin <adam.hawtin@ons.gov.uk>
2021-07-16 12:32:00 +0100

    Add spinner

--- ui/src/SurveyCaseSearch.js
+++ ui/src/SurveyCaseSearch.js
@@ -1,25 +1,31 @@
-import React, { Component } from 'react';
-import '@fontsource/roboto';
-import { Link, Paper, Typography } from '@material-ui/core';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import SurveySimpleSearchInput from './SurveySimpleSearchInput'
-import SurveySampleSearch from './SurveySampleSearch'
-
+import React, { Component } from "react";
+import "@fontsource/roboto";
+import {
+  Box,
+  CircularProgress,
+  Link,
+  Paper,
+  Typography,
+} from "@material-ui/core";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import SurveySimpleSearchInput from "./SurveySimpleSearchInput";
+import SurveySampleSearch from "./SurveySampleSearch";
 
 class SurveyCaseSearch extends Component {
   state = {
     sampleColumns: [],
     caseSearchResults: [],
+    isWaitingForResults: false,
   };
 
   componentDidMount() {
-    this.getSampleColumns()
-    this.setState({ caseSearchResults: this.props.caseSearchResults })
+    this.getSampleColumns();
+    this.setState({ caseSearchResults: this.props.caseSearchResults });
   }
 
   onSearchExecuteAndPopulateList = async (
@@ -27,6 +33,8 @@ class SurveyCaseSearch extends Component {
     searchTerm,
     searchDesc
   ) => {
+    this.setState({ isWaitingForResults: true });
+
     const response = await fetch(searchUrl);
 
     // TODO: We need more elegant error handling throughout the whole application, but this will at least protect temporarily
@@ -36,6 +44,7 @@ class SurveyCaseSearch extends Component {
     }
 
     const matchedCasesJson = await response.json();
+    this.setState({ isWaitingForResults: false });
 
     this.props.onCaseSearchResults(matchedCasesJson, searchTerm, searchDesc);
   };
@@ -59,41 +68,42 @@ class SurveyCaseSearch extends Component {
       .filter((rule) => !rule.sensitive)
       .map((rule) => rule.columnName);
 
-    this.setState({ sampleColumns: nonSensitiveColumns })
-  }
+    this.setState({ sampleColumns: nonSensitiveColumns });
+  };
 
   getCaseCells = (caze) => {
-    const caseId = caze.id
-    let caseCells = []
-    caseCells.push((
+    const caseId = caze.id;
+    let caseCells = [];
+    caseCells.push(
       <TableCell key={0}>
-        <Link
-          onClick={() => this.props.onOpenCaseDetails(caseId)}>
+        <Link onClick={() => this.props.onOpenCaseDetails(caseId)}>
           {caze.caseRef}
         </Link>
       </TableCell>
-    ))
-    caseCells.push(<TableCell key={1}>{caze.collectionExerciseName}</TableCell>)
-    caseCells.push(this.state.sampleColumns.map((sampleColumn, index) => (
-      <TableCell key={index + 2}>{caze.sample[sampleColumn]}</TableCell>
-    )))
+    );
+    caseCells.push(
+      <TableCell key={1}>{caze.collectionExerciseName}</TableCell>
+    );
+    caseCells.push(
+      this.state.sampleColumns.map((sampleColumn, index) => (
+        <TableCell key={index + 2}>{caze.sample[sampleColumn]}</TableCell>
+      ))
+    );
 
     return caseCells;
   };
 
   getTableHeaderRows() {
-    let tableHeaderRows = []
-    tableHeaderRows.push((
-      <TableCell key={0}>Case Ref</TableCell>
-    ))
+    let tableHeaderRows = [];
+    tableHeaderRows.push(<TableCell key={0}>Case Ref</TableCell>);
 
-    tableHeaderRows.push((
-      <TableCell key={1}>Collection Exercise</TableCell>
-    ))
+    tableHeaderRows.push(<TableCell key={1}>Collection Exercise</TableCell>);
 
-    tableHeaderRows.push(this.state.sampleColumns.map((sampleColumn, index) => (
-      <TableCell key={index + 2}>{sampleColumn}</TableCell>
-    )))
+    tableHeaderRows.push(
+      this.state.sampleColumns.map((sampleColumn, index) => (
+        <TableCell key={index + 2}>{sampleColumn}</TableCell>
+      ))
+    );
 
     return tableHeaderRows;
   }
@@ -102,16 +112,14 @@ class SurveyCaseSearch extends Component {
     const tableHeaderRows = this.getTableHeaderRows();
 
     const caseTableRows = this.props.caseSearchResults.map((caze, index) => (
-      <TableRow key={index}>
-        {this.getCaseCells(caze)}
-      </TableRow>
-    ))
+      <TableRow key={index}>{this.getCaseCells(caze)}</TableRow>
+    ));
 
     const borderStyles = {
-      border: '1px solid grey',
-      marginTop: '10px',
-      paddingBottom: "10px"
-    }
+      border: "1px solid grey",
+      marginTop: "10px",
+      paddingBottom: "10px",
+    };
 
     return (
       <div style={{ padding: 20 }}>
@@ -120,64 +128,85 @@ class SurveyCaseSearch extends Component {
         </Typography>
 
         <div style={borderStyles}>
-          <SurveySampleSearch style={borderStyles} surveyId={this.props.surveyId}
+          <SurveySampleSearch
+            style={borderStyles}
+            surveyId={this.props.surveyId}
             onSearchExecuteAndPopulateList={this.onSearchExecuteAndPopulateList}
             searchTermValidator={this.checkWhitespace}
-            collectionExercises={this.props.collectionExercises}>
-          </SurveySampleSearch>
+            collectionExercises={this.props.collectionExercises}
+          ></SurveySampleSearch>
         </div>
 
         <div style={borderStyles}>
-          <SurveySimpleSearchInput style={borderStyles} surveyId={this.props.surveyId}
+          <SurveySimpleSearchInput
+            style={borderStyles}
+            surveyId={this.props.surveyId}
             onSearchExecuteAndPopulateList={this.onSearchExecuteAndPopulateList}
             searchTermValidator={this.isNumeric}
-            urlpathName='caseRef'
-            displayText='Search By Case Ref'
-            searchDesc='Case Ref matching'
+            urlpathName="caseRef"
+            displayText="Search By Case Ref"
+            searchDesc="Case Ref matching"
           />
         </div>
 
         <div style={borderStyles}>
-          <SurveySimpleSearchInput style={borderStyles} surveyId={this.props.surveyId}
+          <SurveySimpleSearchInput
+            style={borderStyles}
+            surveyId={this.props.surveyId}
             onSearchExecuteAndPopulateList={this.onSearchExecuteAndPopulateList}
             searchTermValidator={this.isNumeric}
-            urlpathName='qid'
-            displayText='Search By Qid'
-            searchDesc='cases linked to QID'
+            urlpathName="qid"
+            displayText="Search By Qid"
+            searchDesc="cases linked to QID"
           />
         </div>
-
-        {
-          (this.props.caseSearchTerm) ?
-            <Typography variant="h5" color="inherit" style={{ marginTop: 30, marginBottom: 10 }}>
-              Results for {this.props.caseSearchDesc} "{this.props.caseSearchTerm}":
-            </Typography> :
-            <Typography variant="h5" color="inherit" style={{ marginTop: 30, marginBottom: 10 }}>
-              Make a search
-            </Typography>
-        }
-        {
-          (this.props.caseSearchTerm && this.props.caseSearchResults.length > 0) &&
-          <TableContainer component={Paper} style={{ marginTop: 20 }}>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  {tableHeaderRows}
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {caseTableRows}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        }
-        {
-          (this.props.caseSearchTerm && !this.props.caseSearchResults.length > 0) &&
-          <p>No cases found</p>
-        }
+        {this.state.isWaitingForResults && (
+          <Box
+            margin={4}
+            align="center"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <CircularProgress color="inherit" />
+          </Box>
+        )}
+        {this.props.caseSearchTerm && !this.state.isWaitingForResults && (
+          <Typography
+            variant="h5"
+            color="inherit"
+            style={{ marginTop: 30, marginBottom: 10 }}
+          >
+            Results for {this.props.caseSearchDesc} "{this.props.caseSearchTerm}
+            ":
+          </Typography>
+        )}
+        {!this.props.caseSearchTerm && !this.state.isWaitingForResults && (
+          <Typography
+            variant="h5"
+            color="inherit"
+            style={{ marginTop: 30, marginBottom: 10 }}
+          >
+            Make a search
+          </Typography>
+        )}
+        {this.props.caseSearchTerm &&
+          !this.state.isWaitingForResults &&
+          this.props.caseSearchResults.length > 0 && (
+            <TableContainer component={Paper} style={{ marginTop: 20 }}>
+              <Table>
+                <TableHead>
+                  <TableRow>{tableHeaderRows}</TableRow>
+                </TableHead>
+                <TableBody>{caseTableRows}</TableBody>
+              </Table>
+            </TableContainer>
+          )}
+        {this.props.caseSearchTerm &&
+          !this.state.isWaitingForResults &&
+          !this.props.caseSearchResults.length > 0 && <p>No cases found</p>}
       </div>
-
-    )
+    );
   }
 }
 
