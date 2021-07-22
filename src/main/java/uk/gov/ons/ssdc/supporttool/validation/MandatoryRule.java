e6fc52b6d23717100453faeb10678d68586f7494
Nick Grant <41681172+NickGrantONS@users.noreply.github.com>
2021-07-22 09:29:32 +0100

    Replace deprecated StringUtils.isEmpty with hasText (#34)

--- src/main/java/uk/gov/ons/ssdc/supporttool/validation/MandatoryRule.java
+++ src/main/java/uk/gov/ons/ssdc/supporttool/validation/MandatoryRule.java
@@ -2,13 +2,14 @@ package uk.gov.ons.ssdc.supporttool.validation;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import java.util.Optional;
+import org.springframework.util.StringUtils;
 
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class MandatoryRule implements Rule {
 
   @Override
   public Optional<String> checkValidity(String data) {
-    if (data.strip().isEmpty()) {
+    if (!StringUtils.hasText(data)) {
       return Optional.of("Mandatory value missing");
     }
 
