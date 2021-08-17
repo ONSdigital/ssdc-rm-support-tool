7939d4b2ec2f29eab39af42f1437f305371a33b7
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-08-17 13:02:12 +0100

    Use double pubsub projects

--- src/main/java/uk/gov/ons/ssdc/supporttool/Application.java
+++ src/main/java/uk/gov/ons/ssdc/supporttool/Application.java
@@ -2,8 +2,11 @@ package uk.gov.ons.ssdc.supporttool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gcp.autoconfigure.pubsub.GcpPubSubAutoConfiguration;
+import org.springframework.cloud.gcp.autoconfigure.pubsub.GcpPubSubReactiveAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = {GcpPubSubAutoConfiguration.class, GcpPubSubReactiveAutoConfiguration.class})
 public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
