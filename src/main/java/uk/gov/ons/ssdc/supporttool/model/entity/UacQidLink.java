625dfb71b80b438c07a63c67bb93258f6dd080e7
AdamHawtin <adam.hawtin@ons.gov.uk>
2021-08-18 10:05:25 +0100

    Comment formatting

--- src/main/java/uk/gov/ons/ssdc/supporttool/model/entity/UacQidLink.java
+++ src/main/java/uk/gov/ons/ssdc/supporttool/model/entity/UacQidLink.java
@@ -20,7 +20,7 @@ public class UacQidLink {
   @Id private UUID id;
 
   @Column(
-      name = "qid",  // "name" annotation is required for index on this column to work
+      name = "qid", // "name" annotation is required for index on this column to work
       nullable = false)
   private String qid;
 
