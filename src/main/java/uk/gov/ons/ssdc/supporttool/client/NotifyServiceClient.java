1e299459ea8022bebdd4a0d247dea445222e7163
AdamHawtin <adam.hawtin@ons.gov.uk>
2021-08-31 13:32:54 +0100

    Refactor NotifyServiceClient

--- src/main/java/uk/gov/ons/ssdc/supporttool/client/NotifyServiceClient.java
+++ src/main/java/uk/gov/ons/ssdc/supporttool/client/NotifyServiceClient.java
@@ -1,12 +1,7 @@
 package uk.gov.ons.ssdc.supporttool.client;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.net.URI;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -24,27 +19,17 @@ public class NotifyServiceClient {
   @Value("${notifyservice.connection.port}")
   private String port;
 
-  private static final ObjectMapper objectMapper = new ObjectMapper();
-
   public void requestSmsFulfilment(RequestDTO smsFulfilmentRequest) {
     RestTemplate restTemplate = new RestTemplate();
-    HttpHeaders headers = new HttpHeaders();
-    headers.setContentType(MediaType.APPLICATION_JSON);
-    String requestBody;
-    try {
-      requestBody = objectMapper.writeValueAsString(smsFulfilmentRequest);
-    } catch (JsonProcessingException e) {
-      throw new RuntimeException("Error mapping SMS fulfilment request object to JSON", e);
-    }
-    HttpEntity<String> request = new HttpEntity<>(requestBody, headers);
-    restTemplate.postForEntity(createUri() + "/sms-fulfilment", request, String.class);
+    restTemplate.postForObject(createUri("/sms-fulfilment"), smsFulfilmentRequest, String.class);
   }
 
-  private URI createUri() {
+  private URI createUri(String path) {
     return UriComponentsBuilder.newInstance()
         .scheme(scheme)
         .host(host)
         .port(port)
+        .path(path)
         .build()
         .encode()
         .toUri();
