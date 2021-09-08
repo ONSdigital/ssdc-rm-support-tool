6fad96f81aba0dd2bafffe01f7fe23ed4d4b9130
LeoKHoward <leo-howard@hotmail.co.uk>
2021-09-08 08:18:11 +0100

    More formatting fixes

--- ui/src/CaseDetails.js
+++ ui/src/CaseDetails.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "@fontsource/roboto";
 import {
   Typography,
@@ -17,7 +17,7 @@ import Refusal from "./Refusal";
 import InvalidCase from "./InvalidCase";
 import PrintFulfilment from "./PrintFulfilment";
 import SensitiveData from "./SensitiveData";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import SmsFulfilment from "./SmsFulfilment";
 import JSONPretty from "react-json-pretty";
 
@@ -51,7 +51,7 @@ class CaseDetails extends Component {
 
     const authJson = await response.json();
 
-    this.setState({authorisedActivities: authJson});
+    this.setState({ authorisedActivities: authJson });
   };
 
   getAllBackendData = async () => {
@@ -59,10 +59,10 @@ class CaseDetails extends Component {
     const caseJson = await response.json();
 
     if (response.ok) {
-      this.setState({case: caseJson});
+      this.setState({ case: caseJson });
 
       const uacQidLinksResponse = await fetch(
-          `/api/cases/${this.props.caseId}/uacQidLinks`
+        `/api/cases/${this.props.caseId}/uacQidLinks`
       );
       const uacQidLinksJson = await uacQidLinksResponse.json();
 
@@ -94,241 +94,240 @@ class CaseDetails extends Component {
   };
 
   closeEventDialog = () => {
-    this.setState({eventToShow: null});
+    this.setState({ eventToShow: null });
     // this.setState({ eventToShow: {type : null }});
   };
 
   render() {
     const sortedCaseEvents = this.state.events.sort((first, second) =>
-        first.dateTime.localeCompare(second.dateTime)
+      first.dateTime.localeCompare(second.dateTime)
     );
     sortedCaseEvents.reverse();
 
     const caseEvents = sortedCaseEvents.map((event, index) => (
-        <TableRow key={index}>
-          <TableCell component="th" scope="row">
-            {event.dateTime}
-          </TableCell>
-          <TableCell component="th" scope="row">
-            {event.description}
-          </TableCell>
-          <TableCell component="th" scope="row">
-            {event.source}
-          </TableCell>
-          <TableCell component="th" scope="row">
-            <Button
-                onClick={() => this.openEventPayloadDialog(event)}
-                variant="contained"
-            >
-              {event.type}
-            </Button>
-          </TableCell>
-        </TableRow>
+      <TableRow key={index}>
+        <TableCell component="th" scope="row">
+          {event.dateTime}
+        </TableCell>
+        <TableCell component="th" scope="row">
+          {event.description}
+        </TableCell>
+        <TableCell component="th" scope="row">
+          {event.source}
+        </TableCell>
+        <TableCell component="th" scope="row">
+          <Button
+            onClick={() => this.openEventPayloadDialog(event)}
+            variant="contained"
+          >
+            {event.type}
+          </Button>
+        </TableCell>
+      </TableRow>
     ));
 
     const uacQids = this.state.uacQidLinks.map((uacQidLink, index) => (
-        <TableRow key={index}>
-          <TableCell component="th" scope="row">
-            {uacQidLink.qid}
-          </TableCell>
-          <TableCell component="th" scope="row">
-            {uacQidLink.createdAt}
-          </TableCell>
-          <TableCell component="th" scope="row">
-            {uacQidLink.lastUpdatedAt}
-          </TableCell>
-          <TableCell component="th" scope="row">
-            {uacQidLink.active ? "Yes" : "No"}
-          </TableCell>
-          <TableCell>
-            {this.state.authorisedActivities.includes("DEACTIVATE_UAC") &&
+      <TableRow key={index}>
+        <TableCell component="th" scope="row">
+          {uacQidLink.qid}
+        </TableCell>
+        <TableCell component="th" scope="row">
+          {uacQidLink.createdAt}
+        </TableCell>
+        <TableCell component="th" scope="row">
+          {uacQidLink.lastUpdatedAt}
+        </TableCell>
+        <TableCell component="th" scope="row">
+          {uacQidLink.active ? "Yes" : "No"}
+        </TableCell>
+        <TableCell>
+          {this.state.authorisedActivities.includes("DEACTIVATE_UAC") &&
             uacQidLink.active && (
-                <Button
-                    onClick={() => this.onDeactivate(uacQidLink.qid)}
-                    variant="contained"
-                >
-                  Deactivate
-                </Button>
+              <Button
+                onClick={() => this.onDeactivate(uacQidLink.qid)}
+                variant="contained"
+              >
+                Deactivate
+              </Button>
             )}
-          </TableCell>
-        </TableRow>
+        </TableCell>
+      </TableRow>
     ));
 
     return (
-        <div>
-          <Link to={`/search?surveyId=${this.props.surveyId}`}>
-            ← Back to Search
-          </Link>
-          <Typography variant="h4" color="inherit" style={{marginBottom: 20}}>
-            Case Details
-          </Typography>
-          {this.state.case && (
-              <div>
-                <TableContainer component={Paper} style={{marginTop: 20}}>
-                  <Table>
-                    <TableHead>
-                      <TableRow>
-                        <TableCell>Details</TableCell>
-                        <TableCell align="right">Action</TableCell>
-                      </TableRow>
-                    </TableHead>
-                    <TableBody>
-                      <TableCell component="th" scope="row">
-                        <div>Case ref: {this.state.case.caseRef}</div>
-                        <div>Created at: {this.state.case.createdAt}</div>
-                        <div>Last updated
-                          at: {this.state.case.lastUpdatedAt}</div>
-                        <div>
-                          Receipted:{" "}
-                          {this.state.case.receiptReceived ? "Yes" : "No"}
-                        </div>
-                        <div>
-                          Refused:{" "}
-                          {this.state.case.refusalReceived
-                              ? this.state.case.refusalReceived
-                              : "No"}
-                        </div>
-                        <div>
-                          Invalid: {this.state.case.addressInvalid ? "Yes"
-                            : "No"}
-                        </div>
-                        <div>
-                          Launched EQ:{" "}
-                          {this.state.case.surveyLaunched ? "Yes" : "No"}
-                        </div>
-                      </TableCell>
-                      <TableCell align="right">
-                        {this.state.authorisedActivities.includes(
-                            "CREATE_CASE_REFUSAL"
-                        ) && (
-                            <Refusal
-                                caseId={this.props.caseId}
-                                case={this.state.case}
-                            />
-                        )}
-                        {this.state.authorisedActivities.includes(
-                            "CREATE_CASE_INVALID_CASE"
-                        ) && <InvalidCase caseId={this.props.caseId}/>}
-                        {this.state.authorisedActivities.includes(
-                            "CREATE_CASE_FULFILMENT"
-                        ) && (
-                            <PrintFulfilment
-                                caseId={this.props.caseId}
-                                surveyId={this.props.surveyId}
-                            />
-                        )}
-                        {this.state.authorisedActivities.includes(
-                            "UPDATE_SAMPLE_SENSITIVE"
-                        ) && (
-                            <SensitiveData
-                                caseId={this.props.caseId}
-                                surveyId={this.props.surveyId}
-                            />
-                        )}
-                        {this.state.authorisedActivities.includes(
-                            "CREATE_CASE_SMS_FULFILMENT"
-                        ) && (
-                            <SmsFulfilment
-                                caseId={this.props.caseId}
-                                surveyId={this.props.surveyId}
-                            />
-                        )}
-                      </TableCell>
-                    </TableBody>
-                  </Table>
-                </TableContainer>
-                <TableContainer component={Paper} style={{marginTop: 20}}>
-                  <Table>
-                    <TableHead>
-                      <TableRow>
-                        <TableCell>Date</TableCell>
-                        <TableCell>Description</TableCell>
-                        <TableCell>Source</TableCell>
-                        <TableCell>Event Type</TableCell>
-                      </TableRow>
-                    </TableHead>
-                    <TableBody>{caseEvents}</TableBody>
-                  </Table>
-                </TableContainer>
-                <TableContainer component={Paper} style={{marginTop: 20}}>
-                  <Table>
-                    <TableHead>
-                      <TableRow>
-                        <TableCell>QID</TableCell>
-                        <TableCell>Created At</TableCell>
-                        <TableCell>Last Updated At</TableCell>
-                        <TableCell>Active</TableCell>
-                        <TableCell>Action</TableCell>
-                      </TableRow>
-                    </TableHead>
-                    <TableBody>{uacQids}</TableBody>
-                  </Table>
-                </TableContainer>
-              </div>
-          )}
-          {this.state.eventToShow &&
+      <div>
+        <Link to={`/search?surveyId=${this.props.surveyId}`}>
+          ← Back to Search
+        </Link>
+        <Typography variant="h4" color="inherit" style={{ marginBottom: 20 }}>
+          Case Details
+        </Typography>
+        {this.state.case && (
+          <div>
+            <TableContainer component={Paper} style={{ marginTop: 20 }}>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Details</TableCell>
+                    <TableCell align="right">Action</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  <TableCell component="th" scope="row">
+                    <div>Case ref: {this.state.case.caseRef}</div>
+                    <div>Created at: {this.state.case.createdAt}</div>
+                    <div>Last updated at: {this.state.case.lastUpdatedAt}</div>
+                    <div>
+                      Receipted:{" "}
+                      {this.state.case.receiptReceived ? "Yes" : "No"}
+                    </div>
+                    <div>
+                      Refused:{" "}
+                      {this.state.case.refusalReceived
+                        ? this.state.case.refusalReceived
+                        : "No"}
+                    </div>
+                    <div>
+                      Invalid: {this.state.case.addressInvalid ? "Yes" : "No"}
+                    </div>
+                    <div>
+                      Launched EQ:{" "}
+                      {this.state.case.surveyLaunched ? "Yes" : "No"}
+                    </div>
+                  </TableCell>
+                  <TableCell align="right">
+                    {this.state.authorisedActivities.includes(
+                      "CREATE_CASE_REFUSAL"
+                    ) && (
+                      <Refusal
+                        caseId={this.props.caseId}
+                        case={this.state.case}
+                      />
+                    )}
+                    {this.state.authorisedActivities.includes(
+                      "CREATE_CASE_INVALID_CASE"
+                    ) && <InvalidCase caseId={this.props.caseId} />}
+                    {this.state.authorisedActivities.includes(
+                      "CREATE_CASE_FULFILMENT"
+                    ) && (
+                      <PrintFulfilment
+                        caseId={this.props.caseId}
+                        surveyId={this.props.surveyId}
+                      />
+                    )}
+                    {this.state.authorisedActivities.includes(
+                      "UPDATE_SAMPLE_SENSITIVE"
+                    ) && (
+                      <SensitiveData
+                        caseId={this.props.caseId}
+                        surveyId={this.props.surveyId}
+                      />
+                    )}
+                    {this.state.authorisedActivities.includes(
+                      "CREATE_CASE_SMS_FULFILMENT"
+                    ) && (
+                      <SmsFulfilment
+                        caseId={this.props.caseId}
+                        surveyId={this.props.surveyId}
+                      />
+                    )}
+                  </TableCell>
+                </TableBody>
+              </Table>
+            </TableContainer>
+            <TableContainer component={Paper} style={{ marginTop: 20 }}>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Date</TableCell>
+                    <TableCell>Description</TableCell>
+                    <TableCell>Source</TableCell>
+                    <TableCell>Event Type</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>{caseEvents}</TableBody>
+              </Table>
+            </TableContainer>
+            <TableContainer component={Paper} style={{ marginTop: 20 }}>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>QID</TableCell>
+                    <TableCell>Created At</TableCell>
+                    <TableCell>Last Updated At</TableCell>
+                    <TableCell>Active</TableCell>
+                    <TableCell>Action</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>{uacQids}</TableBody>
+              </Table>
+            </TableContainer>
+          </div>
+        )}
+        {this.state.eventToShow && (
           <Dialog open={true}>
-            <DialogContent style={{padding: 30}}>
+            <DialogContent style={{ padding: 30 }}>
               <div>
                 <Typography
-                    variant="h5"
-                    color="inherit"
-                    style={{margin: 10, padding: 10}}
+                  variant="h5"
+                  color="inherit"
+                  style={{ margin: 10, padding: 10 }}
                 >
                   Event Type: {this.state.eventToShow.type}
                 </Typography>
               </div>
               <div>
                 <Typography
-                    variant="inherit"
-                    color="inherit"
-                    style={{margin: 10, padding: 10}}
+                  variant="inherit"
+                  color="inherit"
+                  style={{ margin: 10, padding: 10 }}
                 >
                   Time of event: {this.state.eventToShow.dateTime}
                 </Typography>
               </div>
               <div>
                 <Typography
-                    variant="inherit"
-                    color="inherit"
-                    style={{margin: 10, padding: 10}}
+                  variant="inherit"
+                  color="inherit"
+                  style={{ margin: 10, padding: 10 }}
                 >
                   Event source: {this.state.eventToShow.source}
                 </Typography>
               </div>
               <div>
                 <Typography
-                    variant="inherit"
-                    color="inherit"
-                    style={{margin: 10, padding: 10}}
+                  variant="inherit"
+                  color="inherit"
+                  style={{ margin: 10, padding: 10 }}
                 >
                   Event channel: {this.state.eventToShow.channel}
                 </Typography>
               </div>
               <div>
                 <Typography
-                    variant="inherit"
-                    color="inherit"
-                    style={{margin: 10, padding: 10}}
+                  variant="inherit"
+                  color="inherit"
+                  style={{ margin: 10, padding: 10 }}
                 >
                   Event payload:
                   <JSONPretty
-                      id="json-pretty"
-                      data={this.state.eventToShow.payload}
-                      style={{margin: 10, padding: 10}}
+                    id="json-pretty"
+                    data={this.state.eventToShow.payload}
+                    style={{ margin: 10, padding: 10 }}
                   ></JSONPretty>
                 </Typography>
               </div>
               <Button
-                  onClick={this.closeEventDialog}
-                  variant="contained"
-                  style={{margin: 10}}
+                onClick={this.closeEventDialog}
+                variant="contained"
+                style={{ margin: 10 }}
               >
                 Cancel
               </Button>
             </DialogContent>
-          </Dialog>}
-        </div>
+          </Dialog>
+        )}
+      </div>
     );
   }
 }
