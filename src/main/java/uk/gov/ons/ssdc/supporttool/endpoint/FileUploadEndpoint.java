51ee3d7ed1f9b4f2d27e8327584a46c4f4a388d8
Luke Loze <Luke.Loze@ext.ons.gov.uk>
2021-09-09 08:00:06 +0100

    replace copied code with better stuff

--- src/main/java/uk/gov/ons/ssdc/supporttool/endpoint/FileUploadEndpoint.java
+++ src/main/java/uk/gov/ons/ssdc/supporttool/endpoint/FileUploadEndpoint.java
@@ -15,28 +15,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import uk.gov.ons.ssdc.supporttool.model.repository.CollectionExerciseRepository;
-import uk.gov.ons.ssdc.supporttool.model.repository.JobRepository;
-import uk.gov.ons.ssdc.supporttool.security.UserIdentity;
 
 @Controller
 public class FileUploadEndpoint {
 
-  private final JobRepository jobRepository;
-  private final UserIdentity userIdentity;
-  private final CollectionExerciseRepository collectionExerciseRepository;
-
   @Value("${file-upload-storage-path}")
   private String fileUploadStoragePath;
-
-  public FileUploadEndpoint(
-      JobRepository jobRepository,
-      UserIdentity userIdentity,
-      CollectionExerciseRepository collectionExerciseRepository) {
-    this.jobRepository = jobRepository;
-    this.userIdentity = userIdentity;
-    this.collectionExerciseRepository = collectionExerciseRepository;
-  }
 
   @PostMapping("/api/upload")
   public ResponseEntity<UUID> handleFileUpload(@RequestParam("file") MultipartFile file) {
@@ -57,7 +41,6 @@ public class FileUploadEndpoint {
         }
 
         fos.write(line.getBytes());
-
         fos.write("\n".getBytes());
       }
 
@@ -68,57 +51,29 @@ public class FileUploadEndpoint {
     return new ResponseEntity<UUID>(fileId, HttpStatus.CREATED);
   }
 
-  private final int[] BYTE_ORDER_MARK = {239, 187, 191};
+  // The mark of the beast: BOM
+  private final byte[] BYTE_ORDER_MARK = {(byte) 239, (byte) 187, (byte) 191};
 
   private String stripBomFromStringIfExists(String stringToCheckAndStrip) throws IOException {
-    int[] firstFewBytes = new int[BYTE_ORDER_MARK.length];
-
     InputStream input = null;
 
     try {
       input = new ByteArrayInputStream(stringToCheckAndStrip.getBytes());
 
-      for (int index = 0; index < BYTE_ORDER_MARK.length; ++index) {
-        firstFewBytes[index] = input.read(); // read a single byte
-      }
+      // Read in the length of a possible BOM in bytes
+      byte[] firstFewBytes = input.readNBytes(BYTE_ORDER_MARK.length);
 
       if (!Arrays.equals(firstFewBytes, BYTE_ORDER_MARK)) {
+        // So not a standard Excel BOM csv, leave it as it is.
         return stringToCheckAndStrip;
       }
+
+      // We've already read the BOM length at the start, so now make into a string
+      return new String(input.readAllBytes());
     } catch (IOException e) {
-      e.printStackTrace();
+      throw new RuntimeException(e);
     } finally {
       input.close();
     }
-
-    return stripBomFromString(stringToCheckAndStrip);
-  }
-
-  private String stripBomFromString(String stringToStrip) throws IOException {
-    long truncatedSize = stringToStrip.getBytes().length - BYTE_ORDER_MARK.length;
-    byte[] memory = new byte[(int) (truncatedSize)];
-    String strippedString;
-
-    InputStream input = null;
-    try {
-      input = new ByteArrayInputStream(stringToStrip.getBytes());
-      input.skip(BYTE_ORDER_MARK.length);
-      int totalBytesReadIntoMemory = 0;
-
-      while (totalBytesReadIntoMemory < truncatedSize) {
-        int bytesRemaining = (int) truncatedSize - totalBytesReadIntoMemory;
-        int bytesRead = input.read(memory, totalBytesReadIntoMemory, bytesRemaining);
-
-        if (bytesRead > 0) {
-          totalBytesReadIntoMemory = totalBytesReadIntoMemory + bytesRead;
-        }
-      }
-
-      strippedString = new String(memory);
-    } finally {
-      input.close();
-    }
-
-    return strippedString;
   }
 }
