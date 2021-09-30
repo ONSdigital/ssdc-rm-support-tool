c2a810a97ac82a86aee83b3e84af3c0ccd395a55
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-09-30 15:25:23 +0100

    Improve testing and fix security bug

--- src/test/java/uk/gov/ons/ssdc/supporttool/endpoint/AllEndpointsIT.java
+++ src/test/java/uk/gov/ons/ssdc/supporttool/endpoint/AllEndpointsIT.java
@@ -1,29 +1,28 @@
 package uk.gov.ons.ssdc.supporttool.endpoint;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.web.client.RestTemplate;
+import uk.gov.ons.ssdc.common.model.entity.UserGroupAuthorisedActivityType;
+import uk.gov.ons.ssdc.supporttool.testhelper.IntegrationTestHelper;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
-public class WhoAmIEndpointIT {
+public class AllEndpointsIT {
+  @Autowired private IntegrationTestHelper integrationTestHelper;
 
   @LocalServerPort private int port;
 
   @Test
-  public void testWhoAmI() {
-    RestTemplate restTemplate = new RestTemplate();
-    String url = "http://localhost:" + port + "/api/whoami";
-    ResponseEntity<Map> response = restTemplate.getForEntity(url, Map.class);
-    assertThat(response.getBody().get("user")).isEqualTo("dummy@fake-email.com");
+  public void testAllTheEndpoints() {
+    integrationTestHelper.testGet(
+        port,
+        UserGroupAuthorisedActivityType.LIST_ACTION_RULES,
+        (bundle) -> String.format("actionRules/?collectionExercise=%s", bundle.getCollexId()));
   }
 }
