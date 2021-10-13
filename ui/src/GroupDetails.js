3fcf57b9b4412f0a03e2bd40a12c6f1dad1a6d2c
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-10-13 16:43:44 +0100

    minor fix

--- ui/src/GroupDetails.js
+++ ui/src/GroupDetails.js
@@ -117,7 +117,7 @@ class GroupDetails extends Component {
 
     // TODO: We need more elegant error handling throughout the whole application, but this will at least protect temporarily
     if (!response.ok) {
-      return;
+      return [];
     }
 
     const responseJson = await response.json();
