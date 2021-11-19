50e1f9439ccd352f87c1e62a8210a01ce1b2bd32
Nick Grant <41681172+NickGrantONS@users.noreply.github.com>
2021-11-19 11:47:12 +0000

    Migrate to shared events version 0.5.0 release (#153)

--- src/main/java/uk/gov/ons/ssdc/supporttool/utility/Constants.java
+++ src/main/java/uk/gov/ons/ssdc/supporttool/utility/Constants.java
@@ -1,5 +1,5 @@
 package uk.gov.ons.ssdc.supporttool.utility;
 
 public class Constants {
-  public static final String EVENT_SCHEMA_VERSION = "v0.3_RELEASE";
+  public static final String EVENT_SCHEMA_VERSION = "0.5.0";
 }
