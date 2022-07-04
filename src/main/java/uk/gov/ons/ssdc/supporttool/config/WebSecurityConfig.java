8fa12ae17d2e189f07532e2a564f1bfcaa1ed2b8
Ryan Grundy <ryan.grundy@ons.gov.uk>
2022-07-04 15:56:35 +0100

    Security headers set in backend instead of frontend

--- src/main/java/uk/gov/ons/ssdc/supporttool/config/WebSecurityConfig.java
+++ src/main/java/uk/gov/ons/ssdc/supporttool/config/WebSecurityConfig.java
@@ -3,6 +3,8 @@ package uk.gov.ons.ssdc.supporttool.config;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
+import org.springframework.security.web.util.matcher.AnyRequestMatcher;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -15,7 +17,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         .deny()
         .contentTypeOptions()
         .and()
-        .httpStrictTransportSecurity();
+        .httpStrictTransportSecurity()
+        .requestMatcher(AnyRequestMatcher.INSTANCE)
+        .includeSubDomains(true)
+        .maxAgeInSeconds(31536000)
+        .and()
+        .referrerPolicy(ReferrerPolicyHeaderWriter.ReferrerPolicy.NO_REFERRER)
+        .and()
+        .contentSecurityPolicy(
+            "default-src 'self'; style-src 'self' 'unsafe-inline' ; upgrade-insecure-requests; block-all-mixed-content");
 
     http.csrf().disable();
   }
