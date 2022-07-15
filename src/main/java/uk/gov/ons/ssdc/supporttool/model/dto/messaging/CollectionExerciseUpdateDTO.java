6b12711a5af66f080e67f3944067b6d09472b3a2
LukeLoze1 <Luke.Loze@ext.ons.gov.uk>
2022-07-15 14:19:33 +0100

    add setCollectionInstrumentSelectionRules to exports Collex

--- src/main/java/uk/gov/ons/ssdc/supporttool/model/dto/messaging/CollectionExerciseUpdateDTO.java
+++ src/main/java/uk/gov/ons/ssdc/supporttool/model/dto/messaging/CollectionExerciseUpdateDTO.java
@@ -3,6 +3,7 @@ package uk.gov.ons.ssdc.supporttool.model.dto.messaging;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 import lombok.Data;
+import uk.gov.ons.ssdc.common.model.entity.CollectionInstrumentSelectionRule;
 
 @Data
 public class CollectionExerciseUpdateDTO {
@@ -13,4 +14,5 @@ public class CollectionExerciseUpdateDTO {
   private OffsetDateTime startDate;
   private OffsetDateTime endDate;
   private Object metadata;
+  private CollectionInstrumentSelectionRule[] collectionInstrumentSelectionRules;
 }
