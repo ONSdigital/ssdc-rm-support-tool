c5c68cc8699d50baf06c603fffcbe550e1541a82
LukeLoze1 <44841627+LukeLoze1@users.noreply.github.com>
2022-10-10 15:52:12 +0100

    remove rasRM from Java bit (#223)

--- src/main/java/uk/gov/ons/ssdc/supporttool/rasrm/model/dto/rest/RasRmSampleSummaryResponseDTO.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.ons.ssdc.supporttool.rasrm.model.dto.rest;
-
-import java.util.UUID;
-import lombok.Data;
-
-@Data
-public class RasRmSampleSummaryResponseDTO {
-  private UUID id;
-}
