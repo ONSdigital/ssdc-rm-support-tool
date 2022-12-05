8d1ef56af170fb34a32aabcd6cf07877cffd22e4
LukeLoze1 <Luke.Loze@ext.ons.gov.uk>
2022-12-05 09:44:35 +0000

    Update SampleUpload.js

--- ui/src/SampleUpload.js
+++ ui/src/SampleUpload.js
@@ -164,11 +164,7 @@ class SampleUpload extends Component {
   onProcessJob = () => {
     fetch(`/api/job/${this.state.selectedJob}/process`, {
       method: "POST",
-    });
-
-    alert(
-      "Posted Process Job successfully, because of complexities this may take a while to update, but don't go mashing the Process Btn"
-    );
+    }).then(() => this.getJobs());
   };
 
   onCancelJob = () => {
