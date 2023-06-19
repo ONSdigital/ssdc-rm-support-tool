184924208c98bfa9a25727e815283937fd7784dc
Liam Toozer <liam.toozer@gmail.com>
2023-06-19 13:02:17 +0100

    Formatting

--- src/main/java/uk/gov/ons/ssdc/supporttool/model/dto/ui/CaseDto.java
+++ src/main/java/uk/gov/ons/ssdc/supporttool/model/dto/ui/CaseDto.java
@@ -3,7 +3,6 @@ package uk.gov.ons.ssdc.supporttool.model.dto.ui;
 import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.Map;
-
 import lombok.Data;
 import uk.gov.ons.ssdc.common.model.entity.RefusalType;
 
